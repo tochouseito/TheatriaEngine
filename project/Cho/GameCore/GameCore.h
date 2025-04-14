b9278ea759eacd4fa004ffdfec0f8c14d50eb98d
toto <k023g0060@g.neec.ac.jp>
2025-04-14 12:14:58 +0900

    テストコミット

--- project/Cho/GameCore/GameCore.h
+++ project/Cho/GameCore/GameCore.h
@@ -25,9 +25,7 @@ public:
 	ObjectContainer* GetObjectContainer() { return m_pObjectContainer.get(); }
 	GameCoreCommand* GetGameCoreCommand() { return m_pGameCoreCommand.get(); }
 	bool IsRunning() const { return isRunning; }
-	// ゲームの実行
 	void GameRun();
-	// ゲームの停止
 	void GameStop();
 private:
 	void CreateSystems(ResourceManager* resourceManager);
