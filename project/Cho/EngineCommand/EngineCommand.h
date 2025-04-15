c31662261ac4319cd6bacb267a08a4596de5e772
toto <k023g0060@g.neec.ac.jp>
2025-04-15 17:20:08 +0900

    エンジンコマンドにコンポーネント追加コマンドを追加

--- project/Cho/EngineCommand/EngineCommand.h
+++ project/Cho/EngineCommand/EngineCommand.h
@@ -1,18 +1,19 @@
 #pragma once
+#include <vector>
 class GameCore;
 class ResourceManager;
 class GraphicsEngine;
 // インタフェース
-//class ICommand
-//{
-//public:
-//	// Constructor
-//	ICommand() = default;
-//	// Destructor
-//	virtual ~ICommand() = default;
-//	virtual void Execute(EngineCommand* edit) = 0;
-//	virtual void Undo(EngineCommand* edit) = 0;
-//};
+class IEngineCommand
+{
+public:
+	// Constructor
+	IEngineCommand() = default;
+	// Destructor
+	virtual ~IEngineCommand() = default;
+	virtual void Execute(EngineCommand* edit) = 0;
+	virtual void Undo(EngineCommand* edit) = 0;
+};
 class EngineCommand
 {
 public:
@@ -27,9 +28,26 @@ public:
 	~EngineCommand()
 	{
 	}
+	// コマンド実行
+	void ExecuteCommand(std::unique_ptr<IEngineCommand> command)
+	{
+		command->Execute(this);
+		m_Commands.push_back(std::move(command));
+	}
+	// コマンド取り消し
+	void UndoCommand()
+	{
+		if (!m_Commands.empty())
+		{
+			m_Commands.back()->Undo(this);
+			m_Commands.pop_back();
+		}
+	}
 private:
 	GameCore* m_GameCore = nullptr;
 	ResourceManager* m_ResourceManager = nullptr;
 	GraphicsEngine* m_GraphicsEngine = nullptr;
+
+	std::vector<std::unique_ptr<IEngineCommand>> m_Commands;
 };
 
--- project/Cho/EngineCommand/EngineCommands.h
+++ project/Cho/EngineCommand/EngineCommands.h
@@ -1,4 +1,119 @@
 #pragma once
+#include <optional>
 #include "EngineCommand/EngineCommand.h"
 
-
+// 3Dオブジェクトを追加するコマンド
+class Add3DObjectCommand :public IEngineCommand
+{
+public:
+	Add3DObjectCommand()
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t objectID;
+};
+// カメラオブジェクトを追加するコマンド
+class AddCameraObjectCommand :public IEngineCommand
+{
+public:
+	AddCameraObjectCommand()
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t objectID;
+};
+// MeshFilterComponentを追加するコマンド
+class AddMeshFilterComponent :public IEngineCommand
+{
+public:
+	AddMeshFilterComponent(const uint32_t& entity) :
+		m_Entity(entity)
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t m_Entity;
+};
+// MeshRendererComponentを追加するコマンド
+class AddMeshRendererComponent :public IEngineCommand
+{
+public:
+	AddMeshRendererComponent(const uint32_t& entity) :
+		m_Entity(entity)
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t m_Entity;
+};
+// MainCameraを設定するコマンド
+class SetMainCamera :public IEngineCommand
+{
+public:
+	SetMainCamera()
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	std::optional<uint32_t> m_PreCameraID = std::nullopt;
+};
+// スクリプトコンポーネントを追加するコマンド
+class AddScriptComponent :public IEngineCommand
+{
+public:
+	AddScriptComponent(const uint32_t& entity) :
+		m_Entity(entity)
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t m_Entity;
+};
+// ラインレンダラーコンポーネントを追加するコマンド
+class AddLineRendererComponent :public IEngineCommand
+{
+public:
+	AddLineRendererComponent(const uint32_t& entity) :
+		m_Entity(entity)
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t m_Entity;
+	uint32_t m_MapID;
+};
+// Rigidbody2Dコンポーネントを追加するコマンド
+class AddRigidbody2DComponent :public IEngineCommand
+{
+public:
+	AddRigidbody2DComponent(const uint32_t& entity) :
+		m_Entity(entity)
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t m_Entity;
+};
+// BoxCollider2Dコンポーネントを追加するコマンド
+class AddBoxCollider2DComponent :public IEngineCommand
+{
+public:
+	AddBoxCollider2DComponent(const uint32_t& entity) :
+		m_Entity(entity)
+	{
+	}
+	void Execute(EngineCommand* edit)override;
+	void Undo(EngineCommand* edit)override;
+private:
+	uint32_t m_Entity;
+};
