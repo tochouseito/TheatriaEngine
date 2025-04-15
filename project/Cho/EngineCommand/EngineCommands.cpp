022aa33c5c8103b100872e2544a5b25efcecaefd
toto <k023g0060@m.neec.ac.jp>
2025-04-16 01:33:56 +0900

    一旦

--- project/Cho/EngineCommand/EngineCommands.cpp
+++ project/Cho/EngineCommand/EngineCommands.cpp
@@ -1,2 +1,70 @@
 #include "pch.h"
 #include "EngineCommands.h"
+
+void Add3DObjectCommand::Execute(EngineCommand* edit)
+{
+}
+
+void Add3DObjectCommand::Undo(EngineCommand* edit)
+{
+}
+
+void AddCameraObjectCommand::Execute(EngineCommand* edit)
+{
+}
+
+void AddCameraObjectCommand::Undo(EngineCommand* edit)
+{
+}
+
+void AddMeshFilterComponent::Execute(EngineCommand* edit)
+{
+}
+
+void AddMeshFilterComponent::Undo(EngineCommand* edit)
+{
+}
+
+void AddMeshRendererComponent::Execute(EngineCommand* edit)
+{
+}
+
+void AddMeshRendererComponent::Undo(EngineCommand* edit)
+{
+}
+
+void SetMainCamera::Execute(EngineCommand* edit)
+{
+}
+
+void SetMainCamera::Undo(EngineCommand* edit)
+{
+}
+
+void AddScriptComponent::Execute(EngineCommand* edit)
+{
+}
+
+void AddScriptComponent::Undo(EngineCommand* edit)
+{
+}
+
+void AddLineRendererComponent::Execute(EngineCommand* edit)
+{
+}
+
+void AddLineRendererComponent::Undo(EngineCommand* edit)
+{
+}
+
+void AddRigidbody2DComponent::Execute(EngineCommand* edit)
+{
+}
+
+void AddRigidbody2DComponent::Undo(EngineCommand* edit)
+{
+}
+
+void AddBoxCollider2DComponent::Execute(EngineCommand* edit)
+{
+}
