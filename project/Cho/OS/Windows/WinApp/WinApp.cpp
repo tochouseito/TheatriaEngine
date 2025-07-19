038dfd6dbcacb32100d4592aed2027fe454b84b2
toto <k023g0060@m.neec.ac.jp>
2025-07-19 20:31:21 +0900

    物理エンジン開発開始

--- project/Cho/OS/Windows/WinApp/WinApp.cpp
+++ project/Cho/OS/Windows/WinApp/WinApp.cpp
@@ -146,10 +146,6 @@ void WinApp::CreateGameWindow() {
 bool WinApp::ProcessMessage() {
 	MSG msg{};
 	// Windowにメッセージが来てたら最優先で処理させる
-	/*if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}*/
 	while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 	{
 		TranslateMessage(&msg);
